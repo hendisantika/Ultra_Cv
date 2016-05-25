b3fbda520aed79c7b8daba8e8412e2005c47c344
hendisantika <hendisantika@yahoo.co.id>
2016-05-25 13:53:10 +0700

    UltraCV in PHP

--- application/models/model_backend.php
+++ application/models/model_backend.php
@@ -1,5 +1,8 @@
 <?php
 //ULTRA CV BY VERLY ANANDA
+//DONATE me : 139 249 6984 a/n elita BCA se ikhlasnya :D for make me keep spirit
+
+//------------------------------------------------//
 class model_backend extends CI_Model{
 
 //cek data 
@@ -17,13 +20,29 @@ class model_backend extends CI_Model{
 		return $this->db->get();
 	}
 
+	function kategori(){
+		$this->db->select('*');
+		$this->db->from('category');
+		return $this->db->get();
+	}
 
 	//FUNCTION FRONT
 
-	function front(){
+	function front(){ //query mewakili ID 
 		$this->db->select('*');
+		$this->db->from('user');
 		$this->db->from('frontend');
 		$this->db->from('frontend_profile');
+		$this->db->from('frontend_footer');
+
+
+		return $this->db->get();
+	}
+
+
+	function query_portos(){
+		$this->db->select('*');
+		$this->db->from('portofolio');
 		return $this->db->get();
 	}
 	//edit front
@@ -33,13 +52,39 @@ class model_backend extends CI_Model{
 	    $this->db->where('id_frontend',$id_frontend);
 		return $this->db->get();
 	}
+	//avatar
+	function avatar_edit($id_login){
+		$this->db->select('*');
+		$this->db->from('user');
+	    $this->db->where('id_user',$id_login);
+		return $this->db->get();
+
+	}
 	function update_front_data ($id_frontend,$front_array){
 		$this->db->where('id_frontend',$id_frontend);
 		return $this->db->update('frontend',$front_array);
 	}
 
-	
+	function update_avatar_data($id_user,$ava_arrai){
+		$this->db->where('id_user',$id_user);
+		return $this->db->update('user',$ava_arrai);
 
+	}
+
+	function update_footer_data($id_footer,$footer_datas){
+		$this->db->where('id_footer',$id_footer);
+		return $this->db->update('frontend_footer',$footer_datas);
+
+	}
+
+//EDIT FOOTER
+function query_footer($id_footer){
+		$this->db->select('*');
+		$this->db->from('frontend_footer');
+	    $this->db->where('id_footer',$id_footer);
+		return $this->db->get();
+
+}
 
 
 	//FUNCTION FRONT_PROFILE
@@ -56,20 +101,63 @@ class model_backend extends CI_Model{
 		return $this->db->update('frontend_profile',$front_array2);
 	}
 
-function blog(){
-		$this->db->select('*');
-		$this->db->from('blog');
-		return $this->db->get();
+function blog($sampai,$dari){
+		$this->db->join('category', 'blog.id_category = category.id_category');
+		$data = $this->db->get('blog', $sampai, $dari);
+		return $data->result();
 	}
 
-	function view($num, $offset)  {
-   
-  $query = $this->db->get("blog",$num, $offset);
-
-  return $query->result();
-  }
-
-	
+		function blog_jumlah(){
+		return $this->db->get('blog')->num_rows();
+	}
 
 
+		function delete_blog_fungsi($id_blog)  {
+			$this->db->where('id_blog',$id_blog);
+			 $query=$this->db->get('blog');
+			 $row = $query->row();
+
+			 unlink("./assets/img_blog/$row->img_blog");
+			 $this->db->delete('blog', array('id_blog' => $id_blog));
+
+		}
+
+		function delete_porto_fungsi($id_porto)  {
+			$this->db->where('id_porto',$id_porto);
+			 $query=$this->db->get('portofolio');
+			 $row = $query->row();
+
+			 unlink("./assets/img_porto/$row->img_porto");
+			 $this->db->delete('portofolio', array('id_porto' => $id_porto));
+
+		
+		}
+
+		function delete_category_fungsi($id_category){
+		 $this->db->delete('category', array('id_category' => $id_category));
+
+
+		}
+
+		function kategori_fungsi_list(){
+		$this->db->select('*');
+		$this->db->from('category');
+		return $this->db->get();
+		}
+
+		function add_category_fungsi(){
+			  $name_category = $this->input->post('name_category');
+         
+
+  $this->db->insert('category',array(
+          
+  						'name_category' => $name_category));
+
+                ///////// END
+
+           redirect ('admin_area/settings_category_blog');
+
+         }
+		
 }
+?>

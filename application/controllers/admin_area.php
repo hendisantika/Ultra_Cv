a2b86bd328f4cb24c79b447ee7b9c8468d00b8a2
hendisantika <hendisantika@yahoo.co.id>
2016-05-27 08:05:18 +0700

    UltraCV using PHP

--- application/controllers/admin_area.php
+++ application/controllers/admin_area.php
@@ -1,10 +1,16 @@
 <?php
 //ULTRA CV BY VERLY ANANDA
+//DONATE me : 139 249 6984 a/n elita BCA se ikhlasnya :D for make me keep spirit and for buy some foods.
+
+//------------------------------------------------//
+
+
 class Admin_area extends CI_Controller{
 	function __construct(){
 		parent::__construct();
 		$this->load->helper(array('url'));
 		$this->load->library('form_validation');
+		 $this->load->library('pagination');
 		$this->cek_session();
 	}
 		function cek_session(){
@@ -19,6 +25,46 @@ class Admin_area extends CI_Controller{
 		$this->load->view('backend/sidebar_admin',$data);
 		$this->load->view('backend/index_admin');
 		$this->load->view('backend/footer_admin');
+	}
+	function logout(){
+		$this->session->sess_destroy();
+		redirect('backend');
+	}
+
+
+//footer frontend
+	function settings_footer(){
+		$this->load->view('backend/header_admin');
+		$id_footer=$this->uri->segment(3);
+		$data['query'] = $this->model_backend->front();
+		$data['data_footer']=$this->model_backend->query_footer($id_footer);
+		$this->load->view('backend/sidebar_admin',$data);
+		$this->load->view('backend/index_footer',$data);
+		$this->load->view('backend/footer_admin');
+
+
+	}
+
+	//portofolio
+
+	function settings_portofolio(){
+		$this->load->view('backend/header_admin');
+		$data['query'] = $this->model_backend->front();
+		$data['query_porto']=$this->model_backend->query_portos();
+		$this->load->view('backend/sidebar_admin',$data);
+		$this->load->view('backend/index_porto',$data);
+		$this->load->view('backend/footer_admin');
+
+	}
+//avatar
+	function settings_avatar(){
+	    $this->load->view('backend/header_admin');
+		$data['query'] = $this->model_backend->front();
+		$id_login=$this->uri->segment(3);
+		$data_avatar['avatar'] = $this->model_backend->avatar_edit($id_login);
+		$this->load->view('backend/sidebar_admin',$data);
+		$this->load->view('backend/index_avatar',$data_avatar);
+		$this->load->view('backend/footer_static');
 	}
 
 	//FRONT
@@ -42,10 +88,23 @@ class Admin_area extends CI_Controller{
 			'link_twit'  => $this->input->post('link_twit'),
 			'link_google'=>	$this->input->post('link_google'));
 		$this->model_backend->update_front_data($id_frontend,$front_array);
-		redirect('index.php/admin_area/settings_front/'.$id_frontend.'  ');
+		redirect('admin_area/settings_front/'.$id_frontend.'  ');
 
 	}
 
+//UPDATE FOOTER
+
+	function update_footer(){
+		$id_footer=$this->uri->segment(3);
+		$footer_datas=array(
+			'notelp'=> $this->input->post('footer_notelp'),
+			'email'    => $this->input->post('footer_email'),
+			'lokasi'  => $this->input->post('footer_lokasi'),
+			'copyright'=>	$this->input->post('footer_copyright'));
+		$this->model_backend->update_footer_data($id_footer,$footer_datas);
+		redirect('admin_area/settings_footer/'.$id_footer.'  ');
+
+	}
 
 	//FRONT PROFILE
 
@@ -71,7 +130,7 @@ class Admin_area extends CI_Controller{
 			'phone' => $this->input->post('phone'),
 			'email' => $this->input->post('email'));
 		$this->model_backend->update_front_profile_data($id_profile,$front_array2);
-		redirect('index.php/admin_area/settings_front_profile/'.$id_profile.'  ');
+		redirect('admin_area/settings_front_profile/'.$id_profile.'  ');
 
 	}
 
@@ -79,12 +138,71 @@ class Admin_area extends CI_Controller{
 	//BLOG
 
 	function settings_blog(){
+
 		$this->load->view('backend/header_admin');
 		$data['query'] = $this->model_backend->front();
-		$data['blog'] = $this->model_backend->blog();
+		$data['kategori'] =$this->model_backend->kategori();
 		$this->load->view('backend/sidebar_admin',$data);
+		$jumlah= $this->model_backend->blog_jumlah();
+
+$config['full_tag_open'] = "<ul class='pagination'>";
+$config['full_tag_close'] ="</ul>";
+$config['num_tag_open'] = '<li>';
+$config['num_tag_close'] = '</li>';
+$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+$config['next_tag_open'] = "<li>";
+$config['next_tagl_close'] = "</li>";
+$config['prev_tag_open'] = "<li>";
+$config['prev_tagl_close'] = "</li>";
+$config['first_tag_open'] = "<li>";
+$config['first_tagl_close'] = "</li>";
+$config['last_tag_open'] = "<li>";
+$config['last_tagl_close'] = "</li>";
+		$config['base_url'] = base_url().'admin_area/settings_blog/';
+		$config['total_rows'] = $jumlah;
+		$config['per_page'] = 5; 		//limit nya brpp disini gans
+ 
+	    $dari = $this->uri->segment('3');
+		$data['blog'] = $this->model_backend->blog($config['per_page'],$dari);
+		$this->pagination->initialize($config); 
 		$this->load->view('backend/index_blog',$data);
 		$this->load->view('backend/footer_static');
 	}
+
+	function settings_category_blog(){
+
+		$this->load->view('backend/header_admin');
+		$data['query'] = $this->model_backend->front();
+		$data['kategori_list'] =$this->model_backend->kategori_fungsi_list();
+		$this->load->view('backend/sidebar_admin',$data);
+		$this->load->view('backend/index_category_list',$data);
+		$this->load->view('backend/footer_static');
+
+	}
+
+	function delete_blog(){
+	$id_blog=$this->uri->segment(3);
+	$this->model_backend->delete_blog_fungsi($id_blog);
+	redirect('admin_area/settings_category_blog');
+}
+
+   function delete_portofolio(){
+	$id_porto=$this->uri->segment(3);
+	$this->model_backend->delete_porto_fungsi($id_porto);
+	redirect('admin_area/settings_portofolio');
+   }
+
+   function delete_category_blog(){
+   	$id_category=$this->uri->segment(3);
+   	$this->model_backend->delete_category_fungsi($id_category);
+	redirect('admin_area/settings_category_blog');
+
+   }
+
+   function add_category(){
+  	$this->model_backend->add_category_fungsi();
+
+   }
 }
 ?>

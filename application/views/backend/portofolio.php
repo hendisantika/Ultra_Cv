f8d721aef79bcd69649a590de2995e80528966a8
hendisantika <hendisantika@yahoo.co.id>
2016-06-04 20:29:09 +0700

    UltraCV using PHP

--- application/views/backend/portofolio.php
+++ application/views/backend/portofolio.php
@@ -1,4 +1,29 @@
 
+<div id="wraper">
+
+    <!-- Start Head section -->
+    <header class="head">
+        <!-- start container -->
+        <div class="container">
+            <!-- start row -->
+            <div class="row">
+                <div class="col-xs-8 col-sm-11 col-lg-11">
+                    <img class="logo-page" src="assets/img/portfolio_l.png" alt="Ukieweb">
+                    <!-- Title Page -->
+                    <h2 class="title">Portfolio</h2>
+                    <!-- Description Page -->
+                    <h4 class="sub-title">Some of My Works</h4>
+                </div>
+                <div class="col-xs-4 col-sm-1 col-lg-1 text-right">
+                    <a href="index-2.html" class="btn-close hover-animate"></a>
+                </div>
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </header>
+    <!-- End Head section -->
+
     <!-- Start Portfolio section -->
     <div class="portfolio-section padding-block">
         <!-- start container -->
@@ -6,24 +31,25 @@
             <!-- start row -->
             <div class="row">
                     <!-- Portfolio -->
-                  <div class="portfolio">
+                    <div class="portfolio">
 
                         <div class="filter_div controls">
                             <div class="col-xs-12 col-sm-12 col-lg-11">
                                 <ul>
-                                    <li class="hover-animate filter active" data-filter="all">All</li>
+                                    <li class="hover-animate filter active" data-filter="all">All Project</li>
+                                
                                 </ul>
                             </div>
                         </div>
-                           <div id="portfolio-grid">
-                        <?php foreach ($portos->result() as $tampiver) {?>
-                    
+
+                        <div id="portfolio-grid">
+
                             <div class="mix col-xs-12 col-sm-6 col-lg-4 portfolio-item category-2">
                                 <div class="within">
 
-                                    <img src="<?php echo base_url('assets/img_porto/'.$tampiver->img_porto.'');?>" alt="Alt">
+                                    <img src="assets/img/portfolio/p1.png" alt="Alt">
                                     <div class="port-item-cont">
-                                        <h3 class="title"><?php echo $tampiver->judul;?></h3>
+                                        <h3 class="title">Project Title</h3>
                                         <p class="desc">Your Project description</p>
                                         <a href="#" class="popup-content view-work hover-animate">View details</a>
                                     </div>
@@ -31,21 +57,24 @@
                                     <div class="hidden">
                                         <div class="podrt-desc">
                                             <div class="modal-box-content">
-                                                <img src="<?php echo base_url('assets/img_porto/'.$tampiver->img_porto.'');?>" alt="Alt">
+                                                <img src="assets/img/portfolio/p_b1.png" alt="Alt">
                                                 <button class="mfp-close"></button>
                                                 <div class="text">
-                                                    <h3 class="title"><?php echo $tampiver->judul;?></h3>
+                                                    <h3 class="title">Project Title</h3>
                                                     <table>
                                                         <tr>
                                                             <td class="font-weight-m width-td">Completed</td>
-                                                            <td><?php echo $tampiver->completed;?></td>
+                                                            <td>November 2014</td>
                                                         </tr>
                                                         <tr>
                                                             <td class="font-weight-m">Client</td>
-                                                            <td><?php echo $tampiver->client;?></td>
+                                                            <td>Envato</td>
                                                         </tr>
                                                     </table>
-                                                    <p><?php echo $tampiver->desc;?>....</p>
+                                                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+                                                        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                                                        sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                                                        laudantium, totam rem aperiamae vitae dicta sunt explicabo.</p>
                                                     <a href="#" class="btn btn-color">See Live</a>
                                                 </div>
                                             </div>
@@ -54,19 +83,16 @@
 
                                 </div>
                             </div>
-                          
-                          <?php      } ?>
-                            
+             
 
 
                         </div>
 
                     </div>
-                           
+                    <!-- end Portfolio -->
             </div>
             <!-- end row -->
         </div>
         <!-- end container -->
     </div>
     <!-- End Portfolio section -->
-
--- application/views/backend/sidebar_admin.php
+++ application/views/backend/sidebar_admin.php
@@ -15,11 +15,13 @@
                       </a>
                       <ul class="sub">
         <?php  foreach($query->result() as $tampil); ?>
-
-                          <li><a  href="<?php echo base_url('index.php/admin_area/settings_front/'.$tampil->id_frontend.' ');?>">Settings Front</a></li>
-                          <li><a  href="<?php echo base_url('index.php/admin_area/settings_front_profile/'.$tampil->id_profile.' ');?>">Settings Profile</a></li>
-                          <li><a  href="panels.html">Settings Portofolio</a></li>
-                          <li><a  href="<?php echo base_url('index.php/admin_area/settings_blog');?>">Kelola Blog</a></li>
+                          <li><a  href="<?php echo base_url('admin_area/settings_avatar/'.$tampil->id_user.'');?>">Settings Avatar account</a></li>
+                          <li><a  href="<?php echo base_url('admin_area/settings_front/'.$tampil->id_frontend.' ');?>">Settings Front</a></li>
+                          <li><a  href="<?php echo base_url('admin_area/settings_front_profile/'.$tampil->id_profile.' ');?>">Settings Front Profile
+                          <li><a  href="<?php echo base_url('admin_area/settings_portofolio/');?>">Kelola Portofolio</a></li>
+                          <li><a  href="<?php echo base_url('admin_area/settings_blog');?>">Kelola Blog</a></li>
+                          <li><a  href="<?php echo base_url('admin_area/settings_category_blog');?>">Kelola Category Blog</a></li>
+                          <li><a  href="<?php echo base_url('admin_area/settings_footer/'.$tampil->id_frontend.'');?>">Settings Footer Frontend</a></li>
                       </ul>
                   </li>
 
